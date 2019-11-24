3e95e77f3f11ee6e6c25990013ddead41fb4c4de
joaobueno16 <jonnyleaobueno@hotmail.com>
2019-11-24 19:13:56 -0300

    arrumei os carros

--- Codigo_pygame.py
+++ Codigo_pygame.py
@@ -10,15 +10,14 @@ import pygame
 import random
 from os import path
 
-
-#diretorios de imagem e som
+# diretorios de imagem e som
 img_dir = path.join(path.dirname(__file__), 'img_dir')
 snd_dir = path.join(path.dirname(__file__), 'snd_dir')
 
-#Dados gerais do jogo.
-WIDTH = 440 # Largura da tela
-HEIGHT = 540 # Altura da tela
-FPS = 60 # Frames por segundo
+# Dados gerais do jogo.
+WIDTH = 440  # Largura da tela
+HEIGHT = 540  # Altura da tela
+FPS = 100  # Frames por segundo
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -33,137 +32,139 @@ pos_car_ini = [70, 190, 350]
 class Player(pygame.sprite.Sprite):
 
     def __init__(self):
-        #construtor da classe pai (Sprite)
+        # construtor da classe pai (Sprite)
         pygame.sprite.Sprite.__init__(self)
 
-        #Carregando imagem de fundo
-        player_img = pygame.image.load(path.join(img_dir, "Player.png")).convert()  
-        
+        # Carregando imagem de fundo
+        player_img = pygame.image.load(path.join(img_dir, "Player.png")).convert()
+
         self.image = player_img
-        #self.image.set_colorkey(BLACK)
+        # self.image.set_colorkey(BLACK)
 
-        #Diminuindo o tamanho da imagem 
-        self.image = pygame.transform.scale(player_img, (1333//25, 2400//25))
+        # Diminuindo o tamanho da imagem
+        self.image = pygame.transform.scale(player_img, (1333 // 25, 2400 // 25))
 
-        #Deixando transparente   
-        #self.image.set_colorkey(BLACK)
+        # Deixando transparente
+        # self.image.set_colorkey(BLACK)
 
-        #Detalhes do posicionamento   
+        # Detalhes do posicionamento
         self.rect = self.image.get_rect()
 
-        #Centraliza no baixo da tela
-        self.rect.centerx = WIDTH / 2
+        # Centraliza no baixo da tela
+        self.rect.centerx = int(WIDTH / 2)
         self.rect.bottom = HEIGHT - 40
 
         # Velocidade do carro
         self.speedx = 0
         self.speedy = 0
-        
 
     # Metodo que atualiza a posição do carro
     def update(self):
         self.rect.x += self.speedx
         self.rect.y += self.speedy
-        
+
         # Mantem dentro da tela
-        if self.rect.right > WIDTH-30:
-            self.rect.right = WIDTH-30
+        if self.rect.right > WIDTH - 30:
+            self.rect.right = WIDTH - 30
         if self.rect.left < 30:
             self.rect.left = 30
         if self.rect.bottom > HEIGHT:
-            self.rect.bottom=HEIGHT
+            self.rect.bottom = HEIGHT
         if self.rect.top < 0:
             self.rect.top = 0
-        
+
 
 # Classe Mob que representa os meteoros
 class Mob(pygame.sprite.Sprite):
-    
+
     # Construtor da classe.
     def __init__(self):
-
-         # Construtor da classe pai (Sprite).
+        # Construtor da classe pai (Sprite).
         pygame.sprite.Sprite.__init__(self)
 
         # Carregando a imagem de fundo.
         mob_img = pygame.image.load(path.join(img_dir, "carro_verde.png")).convert()
-        
-        # Diminuindo o tamanho da imagem.
-        self.image = pygame.transform.scale(mob_img, (1333//25, 2400//25))
-        #self.image.set_colorkey(BLACK)
-        self.rotate = pygame.transform.rotate(mob_img, -180)
-        
 
-            
+        # Diminuindo o tamanho da imagem.
+        self.image = pygame.transform.scale(mob_img, (1333 // 25, 2400 // 25))
+        # self.image.set_colorkey(BLACK)
+        self.rotate = pygame.transform.rotate(mob_img, -180)
+
         # Deixando transparente.
-        #self.image.set_colorkey(BLACK)
-        
+        # self.image.set_colorkey(BLACK)
+
         # Detalhes sobre o posicionamento.
         self.rect = self.image.get_rect()
-       
+
         # Sorteia um lugar inicial em x
-        self.rect.x = pos_car_ini[random.randint(0,2)]
+        self.rect.x = pos_car_ini[random.randint(0, 2)]
         # Sorteia um lugar inicial em y
-        self.rect.y = random.randrange(-150, -30)
+        self.rect.y = random.randrange(-640, -70)
         # Sorteia uma velocidade inicial
-        
-        self.speedy = 6
-        
-    # Metodo que atualiza a posição da navinha
+
+        self.speedy = 15
+
+    # Metodo que atualiza a posição dos carros aleatorios
     def update(self):
-       
         self.rect.y += self.speedy
-        
+
         # Se o carro passar do final da tela, volta para cima
         if self.rect.top > HEIGHT + 10:
-            self.rect.x = pos_car_ini[random.randint(0,2)]
+            self.rect.x = pos_car_ini[random.randint(0, 2)]
             self.rect.y = random.randrange(-150, -30)
             
-            #self.speedy = random.randrange(2, 9)
+        
+
+            # self.speedy = random.randrange(2, 9)
+    #def collide(self , groupcollide):
+        
             
-class Pista(pygame.sprite.Sprite): 
+            
+        
 
-   def __init__(self,x,y):
+class Pista(pygame.sprite.Sprite):
 
-       pygame.sprite.Sprite.__init__(self)
-       background = pygame.image.load(path.join(img_dir, 'pista.png')).convert()
-       self.image = pygame.transform.scale(background,(WIDTH,HEIGHT))
-       self.image.set_colorkey(BLACK)
-       self.rect = self.image.get_rect()
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        background = pygame.image.load(path.join(img_dir, 'pista.png')).convert()
+        self.image = pygame.transform.scale(background, (WIDTH, HEIGHT))
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
 
-       # posicao 
-       self.rect.x = x
-       self.rect.y = y
+        # posicao
+        self.rect.x = x
+        self.rect.y = y
 
-       #velocidade
-       self.speedx = 0
-       self.speedy = 11 
+        # velocidade
+        self.speedx = 0
+        self.speedy = 30
 
-   def update(self):
-       self.rect.y += self.speedy
-       
-       
-       # Se a pista sair de cima da tela, volta para cima
-       if self.rect.y > HEIGHT:
-           self.rect.y=0-HEIGHT+11
-           
+    def update(self):
+        self.rect.y += self.speedy
+
+        # Se a pista sair de cima da tela, volta para cima
+        if self.rect.y > HEIGHT:
+            self.rect.y = 0 - HEIGHT + 30
+
+
 class Telainicial(pygame.sprite.Sprite):
 
-   def __init__(self,x,y):
-       pygame.sprite.Sprite.__init__(self)
-       inicio_img = pygame.image.load(path.join(img_dir, "start.png")).convert()
-       self.image = pygame.transform.scale(inicio_img,(440,540))
-       self.image.set_colorkey(BLACK)
-       self.rect  = self.image.get_rect()
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        inicio_img = pygame.image.load(path.join(img_dir, "start.png")).convert()
+        self.image = pygame.transform.scale(inicio_img, (440, 540))
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
 
-       #posicao
-       self.rect.x = x 
-       self.rect.y = y
+        # posicao
+        self.rect.x = x
+        self.rect.y = y
 
-       self.speedy = 0
+        self.speedy = 0
 
-   def update(self):
-       self.speedy += 0
+    def update(self):
+        self.speedy += 0
+
 
 # Carrega todos os assets uma vez só.
 def load_assets(img_dir, snd_dir):
@@ -180,13 +181,11 @@ pygame.mixer.init()
 assets = load_assets(img_dir, snd_dir)
 # Carrega os sons do jogo
 pygame.mixer.music.load(path.join(snd_dir, '8_Bit_Universe_-_ACDC_Back_In_Black_8_bit_(dj4u.io).mp3'))
-pygame.mixer.music.set_volume(0.4) 
-#music_sound = assets["musica_jogo"] 
+pygame.mixer.music.set_volume(0.4)
+# music_sound = assets["musica_jogo"]
 
 # Loop principal.
 pygame.mixer.music.play(loops=-1)
-
-
 
 # Tamanho da tela.
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -197,26 +196,24 @@ pygame.display.set_caption("Carros")
 # Variável para o ajuste de velocidade
 clock = pygame.time.Clock()
 
-
 # Cria um grupo só da pista
-pista=pygame.sprite.Group()
+pista = pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
 
-pista1 = Pista(0,0)
+pista1 = Pista(0, 0)
 all_sprites.add(pista1)
 pista.add(pista1)
 
 # Carrega o  segundo fundo do jogo
-pista2 = Pista(0,0-HEIGHT)
+pista2 = Pista(0, 0 - HEIGHT)
 all_sprites.add(pista2)
 pista.add(pista2)
-
 
 # Cria uma nave. O construtor será chamado automaticamente.
 player = Player()
 
 # Cria um grupo de todos os sprites e adiciona o player.
-class_player=pygame.sprite.Group()
+class_player = pygame.sprite.Group()
 class_player.add(player)
 all_sprites.add(player)
 
@@ -227,112 +224,111 @@ all_sprites.add(player)
 # Cria um grupo só dos carros aleatorios
 mobs = pygame.sprite.Group()
 
-
 # Cria 8 carros aleatorios e adiciona no grupo carros aleatorios
-for i in range(15):
-    hit2 = pygame.sprite.groupcollide(mobs, mobs, False,False)
-    if len(hit2)<3:
-        
+for i in range(10):
         m = Mob()
         all_sprites.add(m)
         mobs.add(m)
-    
+
+
+
+
 # Comando para evitar travamentos.
 
 try:
     
-    # Loop principal.
+    
+
     running = True
+    state = 2
+
+    player.speedy = 0
+    player.speedx = 0
+    inicial = Telainicial(0, 0)
+    telainicial = pygame.sprite.Group()
+    telainicial.add(inicial)
+    telainicial.draw(screen)
+    telainicial.update()
+    pygame.display.flip()
+    m.speedy = 0
+
+    clock.tick(FPS)
+
     while running:
-        #estados
-        
-        state = 2
-        
-        # Ajusta a velocidade do jogo.
-        clock.tick(FPS)
-        
-        
-        telainicial = pygame.sprite.Group()
-        
-        
-        
-        if state == 2:
-                player.speedy = 0
-                player.speedx = 0
-                inicial = Telainicial(0,0)
-                telainicial = pygame.sprite.Group()
-                telainicial.add(inicial)
-                telainicial.draw(screen) 
-                telainicial.update()
-                pygame.display.flip()
-                m.speedy = 0
-        
-                # Processa os eventos (mouse, teclado, botão, etc).
-                for event in pygame.event.get():
+        for amob in mobs:
+                mobs.remove(amob)
+                hit2=pygame.sprite.spritecollide(amob, mobs, False)
+                mobs.add(amob)
+                if len(hit2)>0:
+                    amob.kill()
+        if len(mobs)<6:
+
+           novo_mob = Mob()        
+           mobs.add(novo_mob)
+           all_sprites.add(novo_mob)
                     
-                    # Verifica se foi fechado.
-                    if event.type == pygame.QUIT:
-                        running = False
-                    # Verifica se apertou alguma tecla.
-                    if event.type == pygame.KEYDOWN:
-                        if event.key == pygame.K_SPACE:   
-                            
-                            state = 1
-                            print('x')
-                            print(state)
-                            if state == 1:
-                                del telainicial
-                                
-                                # Processa os eventos (mouse, teclado, botão, etc).
-                                for event in pygame.event.get():
-                                    
-                                    # Verifica se foi fechado.
-                                    if event.type == pygame.QUIT:
-                                        running = False
-                                        # Dependendo da tecla, altera a velocidade.
-                                    if event.type == pygame.KEDOWN:
-                                        if event.key == pygame.K_LEFT:
-                                            player.speedx = -8
-                                        if event.key == pygame.K_RIGHT:
-                                            player.speedx = 8 
-                                        if event.key == pygame.K_UP:
-                                            player.speedy = -8
-                                        if event.key == pygame.K_DOWN:
-                                            player.speedy = 8
-                                            
-                                    # Verifica se soltou alguma tecla.
-                                    if event.type == pygame.KEYUP:
-                                        # Dependendo da tecla, altera a velocidade.
-                                        if event.key == pygame.K_LEFT:
-                                            player.speedx = 0
-                                        if event.key == pygame.K_RIGHT:
-                                            player.speedx = 0
-                                        if event.key == pygame.K_UP:
-                                            player.speedy = 0
-                                        if event.key == pygame.K_DOWN:
-                                            player.speedy = 0
-                        
-                                    hit = pygame.sprite.groupcollide(mobs, class_player, False,False)
-                                    hit2 = pygame.sprite.groupcollide(mobs, mobs, False,False)
-                                    if len(hit)!= 0:
-                                        pygame.quit()
-                                
-                                             
-                                    # Depois de processar os eventos.
-                                    # Atualiza a acao de cada sprite.
-                                    all_sprites.update()
-                                        
-                                    # A cada loop, redesenha o fundo e os sprites
-                                    screen.fill(BLACK)
-                                    #screen.blit(background, background_rect)
-                                    all_sprites.draw(screen)
-                                    
-                                    # Depois de desenhar tudo, inverte o display.
-                                    pygame.display.flip()
+                    
                 
+                
+
+        clock.tick(FPS)
+
+        if state == 1:
+            
+            
+
+            hit = pygame.sprite.groupcollide(mobs, class_player, False, False)
+            if len(hit) != 0:
+                pygame.quit()
+
+            # Depois de processar os eventos.
+            # Atualiza a acao de cada sprite.
+            all_sprites.update()
+
+            # A cada loop, redesenha o fundo e os sprites
+            screen.fill(BLACK)
+            # screen.blit(background, background_rect)
+            all_sprites.draw(screen)
+
+            # Depois de desenhar tudo, inverte o display.
+            pygame.display.flip()
+
+            # Processa os eventos (mouse, teclado, botão, etc).
+            for event in pygame.event.get():
+
+                # Verifica se foi fechado.
+                if event.type == pygame.QUIT:
+                    running = False
+                    # Dependendo da tecla, altera a velocidade.
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        player.speedx = -8
+                    if event.key == pygame.K_RIGHT:
+                        player.speedx = 8
+                    if event.key == pygame.K_UP:
+                        player.speedy = -8
+                    if event.key == pygame.K_DOWN:
+                        player.speedy = 8
+
+                # Verifica se soltou alguma tecla.
+                if event.type == pygame.KEYUP:
+                    # Dependendo da tecla, altera a velocidade.
+                    if event.key == pygame.K_LEFT:
+                        player.speedx = 0
+                    if event.key == pygame.K_RIGHT:
+                        player.speedx = 0
+                    if event.key == pygame.K_UP:
+                        player.speedy = 0
+                    if event.key == pygame.K_DOWN:
+                        player.speedy = 0
+
+        if state == 2:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT: 
+                    running = False
+                if event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:
+                    if event.key == pygame.K_SPACE:
+                        state = 1
+                        del telainicial
 finally:
     pygame.quit()
-
-
-       
-       
